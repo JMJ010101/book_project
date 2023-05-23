bc7995932f716c92207af733b07ce63f58d8d589
GNchoo <cgn3731@gmail.com>
2023-05-23 16:12:11 +0900

    로그인화면 생성 및 헤더와 연동

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
