4be8c042f4fa2be456867b90857608e82597b68d
jmj010101 <wjdalswn218@naver.com>
2023-05-31 13:48:55 +0900

    추가

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Route path="/find-password" element={<FindPassword />} />
         <Route path="/b2b" element={<B2B />} />
         <Route path="/b2b" element={<B2B />} />
+        <Route path="/b2b" element={<B2B />} />
       </Routes>
     </Router>
   );
