790a5e3800dbfd4905eacfa7bef6b752f767ceab
GNchoo <cgn3731@gmail.com>
2023-06-01 17:28:47 +0900

    검색사이트 추가 카카오 api 연동

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Join from "./pages/Join";
 import FindPassword from "./pages/FindPassword";
 import B2B from "./pages/B2B";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/join" element={<Join />} />
         <Route path="/find-password" element={<FindPassword />} />
         <Route path="/b2b" element={<B2B />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </Router>
   );
---
+++ src/containers/Search/Search.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { bookApi } from "../Book/bookApi";
+import Book from "./Book";
+import * as S from "./searchStyle";
+
+const Search = () => {
+  const [searchData, setSearchData] = useState("");
+  const [data, setData] = useState([]);
+  const [searching, setSearching] = useState(false);
+
+  const changeInput = (e) => {
+    setSearchData(e.target.value);
+  };
+
+  /*
+  useEffect(() => {
+    if (searchData) {
+      bookApi(searchData)
+        .then((res) => {
+          setData(res);
+        })
+        .catch((err) => {
+          alert('err!!');
+        });
+    }
+  }, [searchData]);
+  */
+
+  const searchBook = () => {
+    if (searchData && !searching) {
+      setSearching(true);
+
+      bookApi(searchData)
+        .then((res) => {
+          setData(res);
+        })
+        .catch((err) => {
+          alert("err!!");
+        });
+
+      setSearching(false);
+    }
+  };
+
+  return (
+    <main>
+      <S.Fixed>
+        <S.Header>
+          <S.SearchBar>
+            <input onChange={changeInput} placeholder="책 제목을 입력하세요." />
+            <button onClick={searchBook}>{searching ? "검색 중.." : "검색"}</button>
+          </S.SearchBar>
+        </S.Header>
+      </S.Fixed>
+      <S.BookGrid>
+        {data.map((book) => (
+          <Book key={book.isbn} bookData={book} />
+        ))}
+      </S.BookGrid>
+    </main>
+  );
+};
+
+export default Search;
