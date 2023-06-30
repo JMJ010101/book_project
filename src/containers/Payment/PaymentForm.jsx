2a4b77c3e8bf7d1458ccd90896b64a5c0bee14b6
jmj010101 <wjdalswn218@naver.com>
2023-06-30 17:25:04 +0900

    결제시스템 추가, 필요없는 코드들 삭제

--- src/containers/Payment/PaymentForm.jsx
+++ src/containers/Payment/PaymentForm.jsx
@@ -80,6 +80,40 @@ const PaymentForm = () => {
     setIsModalOpen(false);
   };
 
+  //결제하기
+  function onClickPayment() {
+    /* 1. 가맹점 식별하기 */
+    const { IMP } = window;
+    IMP.init("imp68353505");
+
+    /* 2. 결제 데이터 정의하기 */
+    const data = {
+      pg: "html5_inicis", // PG사
+      pay_method: "card", // 결제수단
+      merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
+      amount: 100, // 결제금액
+      name: "구독서비스", // 주문명
+      buyer_name: "도강규", // 구매자 이름
+      buyer_tel: "01024773482", // 구매자 전화번호
+      buyer_email: "ehrkdrb1@gmail.com", // 구매자 이메일
+    };
+
+    /* 4. 결제 창 호출하기 */
+    IMP.request_pay(data, callback);
+  }
+
+  /* 3. 콜백 함수 정의하기 */
+  function callback(response) {
+    const { success, merchant_uid, error_msg } = response;
+    if (success) {
+      alert("결제 성공");
+      fetchData();
+    } else {
+      alert(`결제 실패: ${error_msg}`);
+    }
+  }
+
+  //구독하기
   const fetchData = async () => {
     try {
       const token = localStorage.getItem("accessToken");
@@ -236,7 +270,7 @@ const PaymentForm = () => {
             <p>0원 결제하기</p>
           </button>
         ) : (
-          <button className="button2" onClick={fetchData}>
+          <button className="button2" onClick={onClickPayment}>
             <p>0원 결제하기</p>
           </button>
         )}
